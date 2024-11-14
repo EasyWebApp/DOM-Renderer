c63a89061fcc49a6d787e4f29ec944dfd358f244
TechQuery <shiy2008@gmail.com>
2024-11-14 10:17:42 +0800

    [fix] Script closing tag

--- source/dist/VDOM.ts
+++ source/dist/VDOM.ts
@@ -125,7 +125,7 @@ export class VNode extends VNodeMeta {
                 }
                 yield `</${tagName}>`;
             } else {
-                yield ` />`;
+                yield tagName === 'script' ? '></script>' : ' />';
             }
         }
     }
